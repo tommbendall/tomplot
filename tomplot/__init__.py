4989e59107d32f8e9c81302f40e86f441584a62b
dw603@exeter.ac.uk <dw603@exeter.ac.uk>
2023-10-09 10:42:26 +0100

    renamed module

--- tomplot/__init__.py
+++ tomplot/__init__.py
@@ -2,9 +2,9 @@ from tomplot.colorbar import *                                            # noqa
 from tomplot.cubed_sphere import *                                        # noqa
 from tomplot.convergence_plot import *                                    # noqa
 from tomplot.data_extraction import *                                     # noqa
+from tomplot.tomplot.data_manipulation import *                           # noqa
 from tomplot.domain import *                                              # noqa
 from tomplot.field_contour_plot import *                                  # noqa
 from tomplot.quiver_plot import *                                         # noqa
 from tomplot.regridding import *                                          # noqa
 from tomplot.tomplot_tools import *                                       # noqa
-from tomplot.local_area_plot import *                                     # noqa
