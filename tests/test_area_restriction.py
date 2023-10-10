0dd197531e77d5062e34a62be04903ab6c4032d7
dw603@exeter.ac.uk <dw603@exeter.ac.uk>
2023-10-10 12:05:44 +0100

    flake8

--- tests/test_area_restriction.py
+++ tests/test_area_restriction.py
@@ -2,15 +2,16 @@
 This tests the routine to restrict Gusto data.
 """
 
-from tomplot import (area_restriction, extract_gusto_field, 
+from tomplot import (area_restriction, extract_gusto_field,
                      extract_gusto_coords, reshape_gusto_data)
 import numpy as np
 import pytest
 from os.path import abspath, dirname
 from netCDF4 import Dataset
 
-@pytest.mark.parametrize('limits', [{'X': (0, 90)}, 
-                                    {'Y': (0, 90)}, 
+
+@pytest.mark.parametrize('limits', [{'X': (0, 90)},
+                                    {'Y': (0, 90)},
                                     {'X': (0, 90), 'Y': (0, 90)}])
 def test_area_restriction(limits):
 
@@ -21,7 +22,6 @@ def test_area_restriction(limits):
     results_file_name = f'{results_dir}/gusto_sphere_3d_field_output.nc'
     data_file = Dataset(results_file_name, 'r')
 
-
     # ------------------------------------------------------------------------ #
     # Extract data
     # ------------------------------------------------------------------------ #
@@ -30,13 +30,13 @@ def test_area_restriction(limits):
     field_full = extract_gusto_field(data_file, field_name, time_idx)
     # Reshape
     _, _, _, coords_Z_full = \
-        reshape_gusto_data(field_full, coords_X_full,
-                            coords_Y_full, coords_Z_full)
-    _, coords_X ,coords_Y = area_restriction(field_full[:,level], 
-                                                      coords_X_full[:,level],
-                                                      coords_Y_full[:,level], 
-                                                      limits) 
-    data_file.close() 
+        reshape_gusto_data(field_full, coords_X_full, coords_Y_full,
+                           coords_Z_full)
+    _, coords_X, coords_Y = area_restriction(field_full[:, level],
+                                             coords_X_full[:, level],
+                                             coords_Y_full[:, level],
+                                             limits)
+    data_file.close()
     # ------------------------------------------------------------------------ #
     # Check the limits have been applied
     # ------------------------------------------------------------------------ #
