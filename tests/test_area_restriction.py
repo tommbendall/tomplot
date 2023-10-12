5ec240f14dbc678d077f5361b0590c6e2ef6396f
Daniel Witt <91120222+Witt-D@users.noreply.github.com>
2023-10-12 16:54:59 +0100

    PR #25: add routine for restricting data to a specific area
    * implemented function
    
    * added import for new function
    
    * flake8 checked
    
    * renamed module
    
    * started on Toms comments
    
    * changed limit input to be a dictionary
    
    * added unit test
    
    * flake8
    
    * flake8
    
    * unit test added
    
    * Update tomplot/data_manipulation.py
    
    Co-authored-by: tommbendall <thomas.bendall@metoffice.gov.uk>
    
    * Update tomplot/data_manipulation.py
    
    Co-authored-by: tommbendall <thomas.bendall@metoffice.gov.uk>
    
    * Update tomplot/data_manipulation.py
    
    Co-authored-by: tommbendall <thomas.bendall@metoffice.gov.uk>
    
    * Update tomplot/data_manipulation.py
    
    Co-authored-by: tommbendall <thomas.bendall@metoffice.gov.uk>
    
    * removed uncecesary variable
    
    * added extra check for incoreect dictionary entry
    
    * fixed lint
    
    * adjusted ValueError tests and error messages2
    
    ---------
    
    Co-authored-by: tommbendall <thomas.bendall@metoffice.gov.uk>

---
+++ tests/test_area_restriction.py
@@ -0,0 +1,48 @@
+"""
+This tests the routine to restrict Gusto data.
+"""
+
+from tomplot import (area_restriction, extract_gusto_field,
+                     extract_gusto_coords, reshape_gusto_data)
+import numpy as np
+import pytest
+from os.path import abspath, dirname
+from netCDF4 import Dataset
+
+
+@pytest.mark.parametrize('limits', [{'X': (0, 90)},
+                                    {'Y': (0, 90)},
+                                    {'X': (0, 90), 'Y': (0, 90)}])
+def test_area_restriction(limits):
+    time_idx = 0
+    level = 0
+    results_dir = f'{abspath(dirname(__file__))}/data'
+    field_name = 'theta'
+    results_file_name = f'{results_dir}/gusto_sphere_3d_field_output.nc'
+    data_file = Dataset(results_file_name, 'r')
+
+    # ------------------------------------------------------------------------ #
+    # Extract data
+    # ------------------------------------------------------------------------ #
+    coords_X_full, coords_Y_full, coords_Z_full = \
+        extract_gusto_coords(data_file, field_name)
+    field_full = extract_gusto_field(data_file, field_name, time_idx)
+    # Reshape
+    field_full, coords_X_full, coords_Y_full, _ = \
+        reshape_gusto_data(field_full, coords_X_full,
+                           coords_Y_full, coords_Z_full)
+
+    _, coords_X, coords_Y = area_restriction(field_full[:, level],
+                                             coords_X_full[:, level],
+                                             coords_Y_full[:, level],
+                                             limits)
+    data_file.close()
+    # ------------------------------------------------------------------------ #
+    # Check the limits have been applied
+    # ------------------------------------------------------------------------ #
+    for key in limits:
+        min, max = limits[key]
+        if key == 'X':
+            assert np.min(coords_X) > min and np.max(coords_X) < max
+        if key == 'Y':
+            assert np.min(coords_Y) > min and np.max(coords_Y) < max
---
+++ tomplot/data_manipulation.py
@@ -0,0 +1,47 @@
+"""
+Common routines for manipulating field data and the corresponding coordinates.
+"""
+import pandas as pd
+import numpy as np
+
+
+def area_restriction(field_data, coords_X, coords_Y, coord_lims):
+    """
+    A function that restricts a data field and returns the new restricted field
+    as well as its corresponding co-ordinates.
+
+    Args:
+        field_data (:class:`numpy.ndarray`): The field to be filtered.
+        coords_X (:class:`numpy.ndarray`): The field along the X axis.
+        coords_Y (:class:`numpy.ndarray`): The field along the Y axis.
+        coord_lims (dict): A dicitionary containing the
+            coordinates as keys and the values are tuples containing the limits.
+
+    Returns:
+        new_field_data (:class:`numpy.ndarray`): The new restricted data.
+        new_X_coords (:class:`numpy.ndarray`): The X coordinates for the restricted data
+        new_Y_coords (:class:`numpy.ndarray`): The Y coordinates for the restricted data
+    """
+
+    if len(np.shape(field_data)) != 1:
+        raise ValueError('area_restriction: input data must be 1D to be filtered by pandas data frame.')
+    if len(np.shape(coords_X)) != 1:
+        raise ValueError('area_restriction: input data must be 1D to be filtered by pandas data frame.')
+    if len(np.shape(coords_Y)) != 1:
+        raise ValueError('area_restriction: input data must be 1D to be filtered by pandas data frame.')
+    if len(coord_lims.keys()) == 0:
+        raise ValueError('area_restriction: limits must be entered as a dictionary with valid axis keys of X or Y')
+
+    data_dict = {'field': field_data, 'X': coords_X, 'Y': coords_Y}
+    df = pd.DataFrame(data_dict)
+    for key in coord_lims:
+        if key not in ('X', 'Y'):
+            raise KeyError(f'Area restriction: coord_lims key {key} not valid. Valid axes are X, Y')
+        min, max = coord_lims[key]
+        df = df[(df[key] >= min) & (df[key] <= max)]
+
+    new_field_data = df['field']
+    new_coords_X = df['X']
+    new_coords_Y = df['Y']
+
+    return (new_field_data, new_coords_X, new_coords_Y)
